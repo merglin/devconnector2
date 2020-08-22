105c05c214e28409489fb7cdba89b4a4bcfbcdf5
Mike Gemmell <merglin@gmail.com>
2020-08-22 16:48:31 -0300

    Add support for logging in

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth");
+const config = require("config");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { check, validationResult } = require("express-validator");
 const User = require("../../models/Users");
 
 // @route GET api/auth
@@ -15,5 +19,61 @@ router.get("/", auth, async (req, res) => {
     return res.status(500).json({ msg: "Unable to locate user." });
   }
 });
+
+// @route POST api/auth
+// @desc Authenticate user
+// @access Public
+router.post(
+  "/",
+  [
+    check("email", "Please include a valid email").isEmail(),
+    check("password").exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      // check if user exists
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({
+          errors: [{ msg: "User not found or incorrect credentials." }],
+        });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res.status(400).json({
+          errors: [{ msg: "User not found or incorrect credentials." }],
+        });
+      }
+
+      // return jsonwebtoken
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
