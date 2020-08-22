d6697d60f875f8f5ed3267f93a8a19d47fce8d4c
Mike Gemmell <merglin@gmail.com>
2020-08-22 12:19:37 -0300

    add placeholder api routes

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ connectDB();
 
 app.get("/", (req, res) => res.send("API Running"));
 
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/auth", require("./routes/api/auth"));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
