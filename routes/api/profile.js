a8cfed6fae5f4b4ccb2fc9b122db147ba8f41588
Mike Gemmell <merglin@gmail.com>
2020-08-24 20:02:46 -0300

    add ability to get github repos for a user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,6 +4,8 @@ const auth = require("../../middleware/auth");
 const User = require("../../models/Users");
 const Profile = require("../../models/Profile");
 const { check, validationResult } = require("express-validator");
+const request = require("request");
+const config = require("config");
 
 const profileprops = [
   "company",
@@ -286,6 +288,34 @@ router.delete("/education/:edu_id", auth, async (req, res) => {
   } catch (error) {
     console.error(error.message);
     return res.status(500).send("Server Error");
+  }
+});
+
+// @route GET api/profile/github/:username
+// @desc Get user repos from github
+// @access Public
+
+router.get("/github/:username", (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+        "githubClientId"
+      )}&client_secret=${config.get("githubClientSecret")}`,
+      method: "GET",
+      headers: { "user-agent": "node.js" },
+    };
+    request(options, (error, response, body) => {
+      if (error) console.error(error);
+      if (response.statusCode !== 200) {
+        return res.status(404).json({ msg: "No github profile found" });
+      }
+      res.json(JSON.parse(body));
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ msg: "Server error" });
   }
 });
 
