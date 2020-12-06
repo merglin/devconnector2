6c8260cb0ddc5b66fa34be0d1656cdbadf9e5cd3
Mike Gemmell <merglin@gmail.com>
2020-12-05 20:59:52 -0400

    Initial implementation of posts view

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,14 +1,30 @@
-import React, { Fragement, useEffect } from "react";
+import React, { Fragment, useEffect } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
+import PostItem from "./PostItem";
 import { getPosts } from "../../actions/post";
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
   useEffect(() => {
     getPosts();
   }, [getPosts]);
-  return <div></div>;
+  return loading ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 className="larger text-primary">
+        <p className="lead">
+          <i className="fas fa-user"></i> Welcome to the community
+        </p>
+      </h1>
+      <div className="posts">
+        {posts.map((post) => (
+          <PostItem key={post._id} post={post} />
+        ))}
+      </div>
+    </Fragment>
+  );
 };
 
 Posts.propTypes = {
