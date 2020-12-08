483e82a8eb24c8f89d699a92d11a854d6f2a851e
Mike Gemmell <merglin@gmail.com>
2020-12-08 19:15:30 -0400

    implement like and unlike

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { setAlert } from "./alert";
-import { GET_POSTS, POST_ERROR } from "./types";
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from "./types";
 
 export const getPosts = () => async (dispatch) => {
   try {
@@ -12,7 +12,46 @@ export const getPosts = () => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: POST_ERROR,
-      payload: { msg: error.reponse.statusText, status: error.response.status },
+      payload: {
+        msg: error.response.statusText,
+        status: error.response.status,
+      },
+    });
+  }
+};
+
+export const addLike = (postId) => async (dispatch) => {
+  try {
+    const res = await axios.put(`api/posts/like/${postId}`);
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { postId, likes: res.data },
+    });
+  } catch (error) {
+    dispatch({
+      type: POST_ERROR,
+      payload: {
+        msg: error.response.statusText,
+        status: error.response.status,
+      },
+    });
+  }
+};
+
+export const removeLike = (postId) => async (dispatch) => {
+  try {
+    const res = await axios.put(`api/posts/unlike/${postId}`);
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { postId, likes: res.data },
+    });
+  } catch (error) {
+    dispatch({
+      type: POST_ERROR,
+      payload: {
+        msg: error.response.statusText,
+        status: error.response.status,
+      },
     });
   }
 };
